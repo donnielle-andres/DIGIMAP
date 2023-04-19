2cf0979c4a097149f41820dd0b0f3fed9e470737
Aki Guanzon <83754370+akiguanzon@users.noreply.github.com>
2023-04-19 22:39:40 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -10,9 +10,11 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -21,33 +23,51 @@ def upload_file():
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
+
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
+
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            newFile = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename), 0)
-            #kernel = np.ones((5, 5), np.float32) / 25
-            #high = cv2.GaussianBlur(newFile, (21, 21), 3) + 127
-            #low = newFile - high
-
+            newFile = cv2.imread(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename), 0)
             equal = cv2.equalizeHist(newFile)
 
-            cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), equal)
-            return redirect(url_for('download_file', name=filename))
+            # save the equalized image with a different filename
+            equalized_filename = 'equalized_' + filename
+            cv2.imwrite(os.path.join(
+                app.config['UPLOAD_FOLDER'], equalized_filename), equal)
+            return render_template('index.html', equalized_filename=equalized_filename, filename=filename)
+
+            # cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), equal)
+            # return redirect(url_for('download_file', name=filename))
+
     return render_template('index.html')
-    
+
+
 @app.route('/uploads/<name>')
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
+
+@app.route('/equalized/<name>')
+def download_equalized_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+
 app.add_url_rule(
     "/files/<name>", endpoint="download_file", build_only=True
+)
+
+app.add_url_rule(
+    "/equalized/<name>", endpoint="download_equalized_file", build_only=True
 )
 
 if __name__ == "__main__":
